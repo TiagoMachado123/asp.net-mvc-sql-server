3c2577f44c46aeee99c2ce2f287ccb4ec107c5e5
TiagoMachado123 <a32135@alunos.ipca.pt>
2026-03-03 16:01:06 +0000

    minor changes

--- teste/Controllers/FaturasController.cs
+++ teste/Controllers/FaturasController.cs
@@ -7,19 +7,16 @@ namespace teste.Controllers
     {
         private readonly IFaturaService _faturaService;
 
-        // Injetamos o nosso Serviço no Controller (tal como fazias no NestJS!)
         public FaturasController(IFaturaService faturaService)
         {
             _faturaService = faturaService;
         }
 
-        // A Action (função) que vai responder quando fores a /Faturas
         public IActionResult Index()
         {
             // 1. Pede ao serviço para ir à base de dados buscar as faturas
             var listaDeFaturas = _faturaService.ObterTodas();
 
-            // 2. Entrega as faturas à View (o HTML)
             return View(listaDeFaturas);
         }
     }
