514f0cc86f004758f0bfc858740deac79ed3e29a
TiagoMachado123 <a32135@alunos.ipca.pt>
2026-03-03 11:41:24 +0000

    Adicionar arquivos de projeto.

---
+++ teste/Models/Fatura.cs
@@ -0,0 +1,10 @@
+﻿namespace teste.Models
+{
+    //Ckass Fatura
+    public class Fatura
+    {
+        public int Id { get; set; }
+        public string NomeCliente { get; set; }
+        public decimal ValorTotal { get; set; } =  0;
+    }
+}
---
+++ teste/Services/FaturaService.cs
@@ -0,0 +1,23 @@
+﻿using teste.Models;
+
+namespace teste.Services
+{
+    public interface IFaturaService
+    {
+        List<Fatura> ObterTodas();
+    }
+
+    public class FaturaService : IFaturaService
+    {
+        public List<Fatura> ObterTodas()
+        {
+            return new List<Fatura>
+            {
+                new Fatura { Id = 1, NomeCliente = "Empresa A", ValorTotal = 1500.50m },
+                new Fatura { Id = 2, NomeCliente = "Empresa B", ValorTotal = 300.00m },
+                new Fatura { Id = 3, NomeCliente = "Cliente Final", ValorTotal = 45.99m },
+                new Fatura { Id = 3, NomeCliente = "Empresa C", ValorTotal = 47.34m }
+            };
+        }
+    }
+}
